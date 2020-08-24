73db367dc673c69a3ba82fdcf812f88ba3c5ff08
Aliaksei Sakalou <nullbsd@gmail.com>
2020-08-24 13:29:58 +0300

    updated easyrsa-3.0.7

--- openvpn_paranoid_installer.sh
+++ openvpn_paranoid_installer.sh
@@ -17,7 +17,7 @@
 ##
 
 ##
-# Copyright (c) 2015-2019 soko1 
+# Copyright (c) 2015-2020 soko1 
 # Copyright (c) 2013 Nyr. Released under the MIT License.
 ##
 
@@ -238,11 +238,11 @@ else
 		yum install openvpn iptables openssl ca-certificates -y
 	fi
 	# Get easy-rsa
-	EASYRSAURL='https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.5/EasyRSA-nix-3.0.5.tgz'
+	EASYRSAURL='https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.7/EasyRSA-3.0.7.tgz'
 	wget -O ~/easyrsa.tgz "$EASYRSAURL" 2>/dev/null || curl -Lo ~/easyrsa.tgz "$EASYRSAURL"
 	tar xzf ~/easyrsa.tgz -C ~/
-	mv ~/EasyRSA-3.0.5/ /etc/openvpn/
-	mv /etc/openvpn/EasyRSA-3.0.5/ /etc/openvpn/easy-rsa/
+	mv ~/EasyRSA-3.0.7/ /etc/openvpn/
+	mv /etc/openvpn/EasyRSA-3.0.7/ /etc/openvpn/easy-rsa/
 	chown -R root:root /etc/openvpn/easy-rsa/
 	rm -f ~/easyrsa.tgz
 	cd /etc/openvpn/easy-rsa/
